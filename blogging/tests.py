efe811325b3c1efef06a5297d409dcd38a0906a1
Zack Connaughton <zaconnaughton@gmail.com>
2019-05-16 21:31:13 -0700

    Added categories for the blog posts and included the category editor in the admin table. cleaned up the plural for category display and included a test to ensure the categories were displaying the correct way

--- blogging/tests.py
+++ blogging/tests.py
@@ -1,6 +1,7 @@
 from django.test import TestCase
 from django.contrib.auth.models import User
 from blogging.models import Post
+from blogging.models import Category
 
 class PostTestCase(TestCase):
     fixtures = ['blogging_test_fixture.json', ]
@@ -12,4 +13,12 @@ class PostTestCase(TestCase):
         expected = "This is a title"
         p1 = Post(title=expected)
         actual = str(p1)
+        self.assertEqual(expected, actual)
+
+class CategoryTestCase(TestCase):
+
+    def test_string_representation(self):
+        expected = "A Category"
+        c1 = Category(name=expected)
+        actual = str(c1)
         self.assertEqual(expected, actual)
--- polling/admin.py
+++ polling/admin.py
@@ -2,5 +2,7 @@ from django.contrib import admin
 
 from django.contrib import admin
 from polling.models import Poll
+from blogging.models import Post, Category
 
 admin.site.register(Poll)
+admin.site.register(Category)
